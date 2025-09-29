ed21d7d15903ad7206efe3aabc0928fad4944e84
Lucas Cordova <lpcordova@outlook.com>
2025-09-29 12:56:47 -0700

    Monday after class

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        int layer = collision.gameObject.layer;
+
+        print("You've completed the game!");
+    }
+
+}
