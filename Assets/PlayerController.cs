91c8e42ed79c097d110430506b356e9a000d4af0
Lucas Cordova <lpcordova@outlook.com>
2025-09-29 11:38:35 -0700

    Monday class prep

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,27 +1,14 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float torqueAmount = 1f;
-    InputAction moveAction;
-    Rigidbody2D rigidbody2D;
     void Start()
     {
-        moveAction = InputSystem.actions.FindAction("Move");
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        
     }
 
     void Update()
     {
-        Vector2 moveVector = moveAction.ReadValue<Vector2>();
-        if (moveVector.x > 0f)
-        {
-            rigidbody2D.AddTorque(-torqueAmount);
-        }
-        else if (moveVector.x < 0f)
-        {
-            rigidbody2D.AddTorque(torqueAmount);
-        }
+        
     }
 }
